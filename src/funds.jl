6df50b0298f0b2c5e5910c8cec434ad8f3d6beb5
Lucas Processi <lucasprocessi@gmail.com>
2021-03-25 17:22:00 -0300

    bugfix new field fund_type after 2020-03

--- src/funds.jl
+++ src/funds.jl
@@ -6,6 +6,7 @@ module Funds
     import ..HTTP
 
     struct FundData
+        fund_type::Union{String, Nothing}
         cnpj::String
         date::Date
         total_value::Float64
@@ -23,11 +24,22 @@ module Funds
 
         out = Vector{FundData}()
 
-        for line in raw[2:end]
+        header = split(String(raw[1]), ";")
+
+        for i in 2:length(raw)
+
+            line = raw[i]
+
+            if line == ""
+                continue
+            end
+
             s = split(String(line), ";")
 
-            if length(s) == 8
+            if ym <= YearMonths.YearMonth(2020,3)
 
+                @assert length(s) == 8 "bad CVM data, this line should have 8 fields: $s"
+                fund_type = nothing
                 cnpj = String(s[1])
                 date = Date(String(s[2]))
                 total_value = parse(Float64, String(s[3]))
@@ -37,14 +49,26 @@ module Funds
                 withdraws = parse(Float64, String(s[7]))
                 number_quota_holders = parse(Int64, String(s[8]))
 
-                d = FundData(
-                    cnpj, date,
-                    total_value, quota_value, net_worth,
-                    deposits, withdraws, number_quota_holders
-                )
-                push!(out, d)
+            else
+                # after 2020-03
+                @assert length(s) == 9 "bad CVM data, this line should have 9 fields: $s"
+                fund_type = String(s[1])
+                cnpj = String(s[2])
+                date = Date(String(s[3]))
+                total_value = parse(Float64, String(s[4]))
+                quota_value = parse(Float64, String(s[5]))
+                net_worth = parse(Float64, String(s[6]))
+                deposits = parse(Float64, String(s[7]))
+                withdraws = parse(Float64, String(s[8]))
+                number_quota_holders = parse(Int64, String(s[9]))
 
             end
+            d = FundData(
+                fund_type, cnpj, date,
+                total_value, quota_value, net_worth,
+                deposits, withdraws, number_quota_holders
+            )
+            push!(out, d)
 
         end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,17 @@ using YearMonths
 using CVMData
 using Test
 
+ym = YearMonths.YearMonth(2020,1)
+quotes = CVMData.Funds.get_month_quotes(ym)
+@test length(quotes) > 0
+
+ym = YearMonths.YearMonth(2020,4)
+quotes = CVMData.Funds.get_month_quotes(ym)
+@test length(quotes) > 0
+
 ym = YearMonths.YearMonth(2021,1)
 quotes = CVMData.Funds.get_month_quotes(ym)
-@test length(quotes) >= 0
+@test length(quotes) > 0
 
-ym = YearMonths.YearMonth(2050,1)
+ym = YearMonths.YearMonth(2999,1)
 @test_throws BoundsError CVMData.Funds.get_month_quotes(ym)
